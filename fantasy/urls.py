67a1123c94756c9773b1040942f58e2f665aff9f
salehmohamed19 <salehrabia87@gmail.com>
2026-09-05 15:32:26 +0300

    add new page for timer and news

--- fantasy/urls.py
+++ fantasy/urls.py
@@ -33,8 +33,8 @@ urlpatterns = [
     path('enter-stats/', views.enter_match_stats, name='enter_match_stats'),
     path('player/<int:player_id>/modal/', views.player_detail_modal, name='player_detail_modal'),
     path('compare/', views.compare_players, name='compare_players'),
-    path('news/', views.news_and_awards, name='news_list'),
-
+    path('news/', views.news_and_awards, name='news_and_awards'),
+    
     # المصادقة والحسابات
     path('login/', LoginView.as_view(template_name='fantasy/login.html', redirect_authenticated_user=True), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
